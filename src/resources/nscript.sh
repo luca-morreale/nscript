b4c2161faaac0b91657aace68f0f9d5365305a27
Stefano Sanfilippo <a.little.coder@gmail.com>
2012-11-04 11:34:42 +0100

    Moved nscript.bat to src/resources/, added SH script.

---
+++ src/resources/nscript.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+#
+# Copyright (C) 2012 Stefano Sanfilippo. See COPYING in the main source
+# package for more information
+#
+
+java -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -jar ./nscript.jar
