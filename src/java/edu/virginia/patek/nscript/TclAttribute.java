eca81cf327a2dd8d58db381ef37c6c3d42a0a06c
Stefano Sanfilippo <a.little.coder@gmail.com>
2012-11-25 15:43:11 +0100

    Ran source code through astyle for consistent formatting. Trimmed spaces and reflowed comments.

--- src/java/edu/virginia/patek/nscript/TclAttribute.java
+++ src/java/edu/virginia/patek/nscript/TclAttribute.java
@@ -1,110 +1,106 @@
-/* --------------------------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
  * NSCRIPT
- * version 1.0.2 
- * Author: 		Enrique Campos-Nanez
+ * version 1.0.2
+ * Author:              Enrique Campos-Nanez
  * Contact information: Dr. Stephen D. Patek
- *			Department of Systems and Information Engineering
- *            		101-C Olsson Hall
- *			Charlottesville, VA 22904
- * 			University of Virginia
- * 			e-mail: patek@virginia.edu
- * -------------------------------------------------------------------------------------------- */
+ *                      Department of Systems and Information Engineering
+ *                      101-C Olsson Hall
+ *                      Charlottesville, VA 22904
+ *                      University of Virginia
+ *                      e-mail: patek@virginia.edu
+ * ------------------------------------------------------------------------- */
 
 package edu.virginia.patek.nscript;
 
 import java.lang.*;
 
 /** An attribute of a TclSnippet. */
-public class TclAttribute extends Object
-{
-	/** The name of the attribute. */
-    String name;
-    /** The default value of the attribute. */
-    String defaultValue; 
-    /** The list available options, as a String. */
-    String options;
-    /** Flags if a class has a default value. */
-    public boolean hasDefault;
-    /** FLags if a class has a finite set of options. */
-    public boolean hasOptions;
-    
-    /** Constructor takes a string and parses the information. 
-     *  @param s a string, usually read from a file. */
-    public TclAttribute( String s )
-    {
-	parseSelf(s);
+public class TclAttribute extends Object {
+  /** The name of the attribute. */
+  String name;
+  /** The default value of the attribute. */
+  String defaultValue;
+  /** The list available options, as a String. */
+  String options;
+  /** Flags if a class has a default value. */
+  public boolean hasDefault;
+  /** FLags if a class has a finite set of options. */
+  public boolean hasOptions;
+
+  /** Constructor takes a string and parses the information.
+   *  @param s a string, usually read from a file. */
+  public TclAttribute( String s )
+  {
+    parseSelf(s);
+  }
+
+  // Access methods
+  /** @return the name  of the attribute. */
+  public String getName( )
+  {
+    return name;
+  }
+
+  /** @return the default value of the attribute. */
+  public String getDefault()
+  {
+    return defaultValue;
+  }
+
+  /** @return the options from the attribute. */
+  public String getOptions()
+  {
+    return options;
+  }
+
+  /** Procedure that parses a string looking for the
+   *  attribute name, its default value, and (optionally) list of options.
+   *  @param s the string containing the information.
+   */
+  void parseSelf( String s )
+  {
+    int defaultIndex, optionsIndex;
+
+    defaultValue = "";
+    options = "";
+
+    s = s.trim();
+    defaultIndex = s.indexOf('=');
+    optionsIndex = s.indexOf(':');
+    if (defaultIndex>=0) {
+      hasDefault = true;
+    } else {
+      hasDefault = false;
+    }
+    if (optionsIndex>=0) {
+      hasOptions = true;
+    } else {
+      hasOptions = false;
     }
 
-    // Access methods
-    /** @return the name  of the attribute. */
-    public String getName( )
-    {
-	return name;
-    }
-    
-    /** @return the default value of the attribute. */
-    public String getDefault()
-    {
-	return defaultValue;
-    }
+    // Ok, now read
+    if (hasDefault) {
+      name = s.substring(0,defaultIndex);
+      if (hasOptions) {
+        defaultValue = s.substring(defaultIndex+1,optionsIndex);
+        options = s.substring(optionsIndex+1);
+      } else {
 
-	/** @return the options from the attribute. */
-    public String getOptions()
-    {
-	return options;
+        defaultValue = s.substring(defaultIndex+1);
+      }
+    } else {
+      if (hasOptions) {
+        name = s.substring(0,optionsIndex);
+        options = s.substring(optionsIndex+1);
+      } else {
+        name = s;
+      }
     }
+  }
 
-	/** Procedure that parses a string looking for the 
-	 *  attribute name, its default value, and (optionally) list of options. 
-	 *  @param s the string containing the information. */
-    void parseSelf( String s )
-    {
-	int defaultIndex, optionsIndex;
-
-	defaultValue = "";
-	options = "";
-
-	s = s.trim();
-	defaultIndex = s.indexOf('=');
-	optionsIndex = s.indexOf(':');
-	if (defaultIndex>=0) {
-	    hasDefault = true;
-	} else {
-	    hasDefault = false;
-	}
-	if (optionsIndex>=0) {
-	    hasOptions = true;
-	} else {
-	    hasOptions = false;
-	}
-	
-	// Ok, now read
-	if (hasDefault) {
-	    name = s.substring(0,defaultIndex);
-	    if (hasOptions) { 
-		defaultValue = s.substring(defaultIndex+1,optionsIndex);
-		options = s.substring(optionsIndex+1);
-	    } else {
-		
-		defaultValue = s.substring(defaultIndex+1);
-	    }
-	} else {
-	    if (hasOptions) {
-		name = s.substring(0,optionsIndex);
-		options = s.substring(optionsIndex+1);
-	    } else {
-		name = s;
-	    }
-	}
-    }
-    
-    /** Puts the attribute information as a single string. */
-    public String toString()
-    {
-	return name + " = " + defaultValue + " : " + options;
-    }
+  /** Puts the attribute information as a single string. */
+  public String toString()
+  {
+    return name + " = " + defaultValue + " : " + options;
+  }
 }
-
-
-
-
--- src/java/edu/virginia/patek/nscript/TclLibraryManager.java
+++ src/java/edu/virginia/patek/nscript/TclLibraryManager.java
@@ -1,14 +1,14 @@
-/* --------------------------------------------------------------------------------------------
+/* ----------------------------------------------------------------------------
  * NSCRIPT
- * version 1.0.2 
- * Author: 		Enrique Campos-Nanez
+ * version 1.0.2
+ * Author:              Enrique Campos-Nanez
  * Contact information: Dr. Stephen D. Patek
- *			Department of Systems and Information Engineering
- *            		101-C Olsson Hall
- *			Charlottesville, VA 22904
- * 			University of Virginia
- * 			e-mail: patek@virginia.edu
- * -------------------------------------------------------------------------------------------- */
+ *                      Department of Systems and Information Engineering
+ *                      101-C Olsson Hall
+ *                      Charlottesville, VA 22904
+ *                      University of Virginia
+ *                      e-mail: patek@virginia.edu
+ * ------------------------------------------------------------------------- */
 
 package edu.virginia.patek.nscript;
 
@@ -18,170 +18,179 @@ import edu.virginia.patek.nscript.TclSnippet;
 import java.util.*;
 import java.io.*;
 
-/** Manages the set of opened libraries in the environment. Among other things, the
- *  TclLibraryManager is responsible for verifying unique TclSnippet names to
- *  prevent naming conflicts, verify uniqueness of library names, find a reference, 
- *  and finding a snippet, either by name or entry number. */
-public class TclLibraryManager extends Object
-{
-	/** The collection of libraries. */
-    ArrayList libraries;
+/** Manages the set of opened libraries in the environment. Among other things,
+ *  the TclLibraryManager is responsible for verifying unique TclSnippet names
+ *  to prevent naming conflicts, verify uniqueness of library names, find a
+ *  reference, and finding a snippet, either by name or entry number.
+ */
+public class TclLibraryManager extends Object {
+  /** The collection of libraries. */
+  ArrayList libraries;
 
-	/** Default constructor, creates the container for libraries. */
-    public TclLibraryManager()
-    {
-		libraries = new ArrayList();
-    }
+  /** Default constructor, creates the container for libraries. */
+  public TclLibraryManager()
+  {
+    libraries = new ArrayList();
+  }
 
-	/** Verifies if a given name is already being used for a snippet. 
-	 *  @param snippetName the name of the snippet. 
-	 *  @return true if the supplied name is being used by any opened snippet, false otherwise. */
-    public boolean snippetNameExists(String snippetName)
-    {
-		TclLibrary l;
-		Iterator i;
-	
-		i = libraries.iterator();
-		while (i.hasNext()) {
-			l = (TclLibrary) i.next();
-			if (l.nameExists(snippetName))
-			return true;
-		}
-		return false;
-    }
+  /** Verifies if a given name is already being used for a snippet.
+   *  @param snippetName the name of the snippet.
+   *  @return true if the supplied name is being used by any opened snippet,
+   *          false otherwise.
+   */
+  public boolean snippetNameExists(String snippetName)
+  {
+    TclLibrary l;
+    Iterator i;
 
-	/** Verifies if a given library name is already loaded in the library. 
-	 *  @param libName the name to verify. 
-	 *  @return true if the supplied name is being taken by any of the opened libraries, false
-	 *  otherwise. */
-    public boolean libNameExists(String libName)
-    {
-		TclLibrary l;
-		Iterator i;
-	
-		i = libraries.iterator();
-		while (i.hasNext()) {
-			l = (TclLibrary) i.next();
-			if (libName.equals(l.getName()))
-			return true;
-		}
-		return false;
+    i = libraries.iterator();
+    while (i.hasNext()) {
+      l = (TclLibrary) i.next();
+      if (l.nameExists(snippetName))
+        return true;
     }
+    return false;
+  }
 
-	/** Adds a new library using the fileName. This method will try to opened the given
-	 *  file, and parse the library from it, creating a new library object, which then will
-	 *  be added to the library manager collection.
-	 *  @param fileName the file location. 
-	 *  @return true if the file was succesfully opened and library correctly parsed, false
-	 *  otherwise. */
-    public boolean addLibrary( String fileName )
-    {
-		File f;
-		FileReader fr;
-		BufferedReader br;
-		TclLibrary tl;
-		int i;
-		boolean go;
-	
-		try {
-			f = new File(fileName);
-			fr = new FileReader(f);
-			br = new BufferedReader(fr);
-			tl = new TclLibrary( br );
-			go = true;
-			go = !libNameExists(tl.getName());
-			for (i=0;i<tl.getSnippetCount();i++) {
-			if (snippetNameExists(tl.getSnippet(i).getName()))
-				go = false;
-			}
-			if (go) {
-			libraries.add( tl );
-			return true;
-			} else 
-			return false;
-		} catch (FileNotFoundException e) {
-			System.out.println("Library file not found:"+e.toString());
-			return false;
-		}
-    }
+  /** Verifies if a given library name is already loaded in the library.
+   *  @param libName the name to verify.
+   *  @return true if the supplied name is being taken by any of the
+   *          opened libraries, false otherwise.
+   */
+  public boolean libNameExists(String libName)
+  {
+    TclLibrary l;
+    Iterator i;
 
-	/** Returns the number of opened libraries. 
-	 *  @return the number of opened libraries. */
-    public int getLibraryCount()
-    {
-		return libraries.size();
+    i = libraries.iterator();
+    while (i.hasNext()) {
+      l = (TclLibrary) i.next();
+      if (libName.equals(l.getName()))
+        return true;
     }
+    return false;
+  }
 
-	/** Obtains a library by its reference number. 
-	 *  @param inIndex 0-based index of the library. 
-	 *  @return a reference to the library. Note that if the index is incorrect
-	 *  NULL is returned. */
-    public TclLibrary getLibrary( int inIndex )
-    {
-		if (inIndex >=0 && inIndex<libraries.size())
-			return (TclLibrary)libraries.get(inIndex);
-		else
-			return null;
-    }
+  /** Adds a new library using the fileName. This method will try to
+   *  opened the given file, and parse the library from it, creating a
+   *  new library object, which then will be added to the library manager
+   *  collection.
+   *
+   *  @param fileName the file location.
+   *  @return true if the file was succesfully opened and library
+   *          correctly parsed, false otherwise.
+   */
+  public boolean addLibrary( String fileName )
+  {
+    File f;
+    FileReader fr;
+    BufferedReader br;
+    TclLibrary tl;
+    int i;
+    boolean go;
 
-	/** Obtains a library by its name. 
-	 *  @param inLibName the name of the library.
-	 *  @return a reference to the library with the requested name, NULL if the name does not
-	 *  exist. */
-    public TclLibrary getLibrary( String inLibName )
-    {
-		Iterator i;
-		TclLibrary l;
-	
-		i = libraries.iterator();
-		while (i.hasNext()) {
-			l = (TclLibrary)i.next();
-			if (inLibName.equals(l.getName()))
-			return l;
-		}
-		return null;
+    try {
+      f = new File(fileName);
+      fr = new FileReader(f);
+      br = new BufferedReader(fr);
+      tl = new TclLibrary( br );
+      go = true;
+      go = !libNameExists(tl.getName());
+      for (i=0; i<tl.getSnippetCount(); i++) {
+        if (snippetNameExists(tl.getSnippet(i).getName()))
+          go = false;
+      }
+      if (go) {
+        libraries.add( tl );
+        return true;
+      } else
+        return false;
+    } catch (FileNotFoundException e) {
+      System.out.println("Library file not found:"+e.toString());
+      return false;
     }
+  }
 
-	/** Obtains a snippet by providing the reference to the library, and a reference index
-	 *  to the snippen within the library. 
-	 *  @param inLibIndex the 0-based index of the library of interest. 
-	 *  @param inSnippetIndex the 0-based index of the snippet within the library. 
-	 *  @return a reference to the requested snippet if the references are correct, NULL
-	 *  otherwise. */
-    public TclSnippet getSnippet( int inLibIndex, int inSnippetIndex)
-    {
-		TclLibrary l;
-	
-		if (inLibIndex>=0 && inLibIndex<libraries.size()) {
-			l = (TclLibrary)libraries.get(inLibIndex);
-			if (inSnippetIndex>=0 && inSnippetIndex<l.getSnippetCount())
-			return l.getSnippet(inSnippetIndex);
-			else
-			return null;
-		} else {
-			return null;
-		}
-    }
+  /** Returns the number of opened libraries.
+   *  @return the number of opened libraries. */
+  public int getLibraryCount()
+  {
+    return libraries.size();
+  }
 
-	/** Obtains a snippet by its name. The method will look in every opened library
-	 *  for a library, and return a reference to the requested snippet if found. 
-	 *  @param sName the name of the requested snippet. 
-	 *  @return a reference to the snippet if the name is found, NULL otherwise. */
-    public TclSnippet getSnippet( String sName )
-    {
-		Iterator i = libraries.iterator();
-		TclLibrary l;
-		TclSnippet s;
-	
-		while (i.hasNext()) {
-			l = (TclLibrary)i.next();
-			s = l.getSnippet(sName);
-			if (s!=null)
-			return s;
-		}
-		return null;
+  /** Obtains a library by its reference number.
+   *  @param inIndex 0-based index of the library.
+   *  @return a reference to the library. Note that if the index is incorrect
+   *  NULL is returned. */
+  public TclLibrary getLibrary( int inIndex )
+  {
+    if (inIndex >=0 && inIndex<libraries.size())
+      return (TclLibrary)libraries.get(inIndex);
+    else
+      return null;
+  }
+
+  /** Obtains a library by its name.
+   *  @param inLibName the name of the library.
+   *  @return a reference to the library with the requested name,
+   *          NULL if the name does not exist.
+   */
+  public TclLibrary getLibrary( String inLibName )
+  {
+    Iterator i;
+    TclLibrary l;
+
+    i = libraries.iterator();
+    while (i.hasNext()) {
+      l = (TclLibrary)i.next();
+      if (inLibName.equals(l.getName()))
+        return l;
     }
+    return null;
+  }
+
+  /** Obtains a snippet by providing the reference to the library, and a
+   *  reference index to the snippen within the library.
+   *  @param inLibIndex the 0-based index of the library of interest.
+   *  @param inSnippetIndex the 0-based index of the snippet within the
+   *                        library.
+   *  @return a reference to the requested snippet if the references are
+   *          correct, NULL otherwise.
+   */
+  public TclSnippet getSnippet( int inLibIndex, int inSnippetIndex)
+  {
+    TclLibrary l;
+
+    if (inLibIndex>=0 && inLibIndex<libraries.size()) {
+      l = (TclLibrary)libraries.get(inLibIndex);
+      if (inSnippetIndex>=0 && inSnippetIndex<l.getSnippetCount())
+        return l.getSnippet(inSnippetIndex);
+      else
+        return null;
+    } else {
+      return null;
+    }
+  }
+
+  /** Obtains a snippet by its name. The method will look in every opened
+   *  library for a library, and return a reference to the requested
+   *  snippet if found.
+   *
+   *  @param sName the name of the requested snippet.
+   *  @return a reference to the snippet if the name is found, NULL otherwise.
+   */
+  public TclSnippet getSnippet( String sName )
+  {
+    Iterator i = libraries.iterator();
+    TclLibrary l;
+    TclSnippet s;
+
+    while (i.hasNext()) {
+      l = (TclLibrary)i.next();
+      s = l.getSnippet(sName);
+      if (s!=null)
+        return s;
+    }
+    return null;
+  }
 }
-
-
-
